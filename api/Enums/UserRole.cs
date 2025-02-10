57b616f3b3d4e19609448977725ae34bb44a9cf0
kuandearu <140840612+kuandearu@users.noreply.github.com>
2025-02-11 03:05:54 +0700

    Delete api directory

--- api/Enums/UserRole.cs
+++
@@ -1,9 +0,0 @@
-namespace api.Enums
-{
-    public enum UserRole
-    {
-        RegisteredUser = 1, // Authenticated users with basic privileges like saving favorites or posting comments or buying.
-        Moderator = 2,      // Users responsible for moderating content (e.g., approving or removing posts).
-        Admin = 3,  
-    }
-}
